4a93651d88e1b5baeab346174635f7a78796422f
Amit Barjatya <ameetbarjatya@gmail.com>
2016-08-02 16:44:58 -0700

    Exploring Repeat Feature of AngularJS

---
+++ js/namesController.js
@@ -0,0 +1,19 @@
+angular.module('myApp', []).controller('namesCtrl', function($scope) {
+    $scope.names = [
+        {name:'Jani',country:'Norway'},
+        {name:'Hege',country:'Sweden'},
+        {name:'Kai',country:'Denmark'}
+    ];
+
+    $scope.xnames = [
+        'Jani',
+        'Carl',
+        'Margareth',
+        'Hege',
+        'Joe',
+        'Gustav',
+        'Birgit',
+        'Mary',
+        'Kai'
+    ];
+}); 
